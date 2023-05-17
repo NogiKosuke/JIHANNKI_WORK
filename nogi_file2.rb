b23f82e873a258acea482785be3a2289b3c3dffa
mysthkr4036@gmail.com <hikaru_miyashita>
2023-05-17 17:16:51 +0900

    add function of Output files

--- nogi_file2.rb
+++ nogi_file2.rb
@@ -1,15 +1,18 @@
 # 在庫データを文字列として受け取りそれをJSONのデータ構造を利用してJSONが求める形に変換してハッシュ化する
 require 'json'
-file = File.open("nogi_stock2.txt", "r")
-str = file.read
 
-puts str
+def read_stock 
+    file = File.open("nogi_stock2.txt", "r")
+    str = file.read
 
-str = str.gsub(/:(\w+)=>/)do
-    "\"#{$1}\": "
+    puts str
+
+    str = str.gsub(/:(\w+)=>/)do
+        "\"#{$1}\": "
+    end
+
+    puts str
+
+    hash = JSON.parse(str, symbolize_names: true)
+    puts hash [:coke]
 end
-
-puts str
-
-hash = JSON.parse(str, symbolize_names: true)
-puts hash [:coke]
